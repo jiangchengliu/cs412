20ef8dab457e05e69622d422ff5e3da6a6ed72be
John Liu <jiangcl@bu.edu>
2024-10-05 23:55:50 -0400

    mini_fb added city

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -7,6 +7,7 @@ class Profile(models.Model):
     last_name = models.CharField(max_length=20)
     email = models.EmailField()
     image_url = models.URLField()
+    city = models.CharField(max_length=20, blank=True)
     
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ShowAllProfiles
 
 urlpatterns = [
-    path("show_all_profiles", ShowAllProfiles.as_view(), name="show_all_profiles"),
+    path("", ShowAllProfiles.as_view(), name="show_all_profiles"),
 ]
